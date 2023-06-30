8d51bad55e62769692d17210430610f3550519c5
confidencer <confidencesmithjason0219@gmail.com>
2023-06-30 14:14:58 +0900

    Add slug Field

--- members/urls.py
+++ members/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/details/<int:id>',views.details, name = 'details'),
+    path('members/details/<slug:slug>',views.details, name = 'details'),
     path('members/', views.members, name= 'members'),
     path('', views.main, name='main'),
     path('testing/', views.testing ,name='testing'),
